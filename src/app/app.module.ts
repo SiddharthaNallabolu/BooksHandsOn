ded6779b796873c897707827548e1c49837d352e
SiddharthaNallabolu <siddharthanallabolu@gmail.com>
2021-06-29 15:37:34 +0530

    Till implementing routes(Till Route Configuration)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { PopularBooksComponent } from './popular-books/popular-books.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AuthorsComponent,
+    PopularBooksComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'books', component: BooksComponent },
+      { path: 'authors', component: AuthorsComponent },
+      { path: 'popular-books', component: PopularBooksComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', component: AppComponent }
+    ]),
     AppRoutingModule
   ],
   providers: [],
